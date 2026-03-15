494e484f5175105549c573f81905fa5c9f96263f
isalu <isabellalucena15@gmail.com>
2026-03-15 01:09:42 -0300

    feat (estudo-projeto): Implementação do Analisador Léxico com AFDs estrutura básica e adição de arquivos de teste para entender funcionamento da leitura de linha. Diagrama para entender o Lexer.

---
+++ dfa.py
@@ -0,0 +1,31 @@
+#maquina de estados finitos 
+# Implementação do Analisador léxico usando Autômatos Finitos Determinísticos (AFDs), com cada estado como uma função.
+
+def estado_inicial(linha, index, tokens):
+    """
+    Estado inicial é responsável analisar o caractere atual e decidir qual estado seguir com base no tipo do caractere (número, letra, operador, etc.).
+    Args:
+        linha (list): A linha de código a ser analisada
+        index (int): O índice atual na linha
+        tokens (list): A lista de tokens encontrados até o momento
+    """
+    return
+
+def estado_numero(linha, index, tokens):
+    return
+
+def estado_palavra(linha, index, tokens):
+    return
+
+def estado_operador(linha, index, tokens):
+    return
+
+def estado_parenteses(linha, index, tokens):
+    return
+
+def estado_falha(linha, index, tokens):
+    return
+
+def estado_especiais(linha, index, tokens):
+    return
+
---
+++ lexer_to_assembly.py
@@ -0,0 +1,36 @@
+#Isabella Lucena Conceição
+#ra1_4
+from fileinput import filename
+import sys
+import dfa
+
+#TODO O vetor de tokens gerado pelo Analisador Léxico deve ser salvo em um arquivo .txt para uso nas próximas fases do projeto.
+
+#recebe o nome do arquivo de teste
+def recieve_file_name():
+    if len(sys.argv) != 2:
+        print("Usage: python lexer.py <filename>.txt")
+        exit(1)
+    return sys.argv[1]
+
+#processa o arquivo e retorna um vetor de linhas para irem ao parser
+def process_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
+
+#responsável por reconhecer pedaços VÁLIDOS do código não fazer verificação da operação de expressão
+# se recebe (1.0 2.0 +) deve reconhecer os tokens [( , 1.0, 2.0, +, )]
+#aqui passa o ADF para reconhecer os tokens válidos e formar a expressão a ser passada para o parser que será [( , 1.0, 2.0, +, )]
+def parseExpressao(linha:str) -> list[str]:
+    tokens = []
+    index = 0
+    while index < len(linha):
+        index = dfa.estado_inicial(linha,index, tokens) 
+    return tokens
+
+filename = recieve_file_name()
+lines = process_file(filename)
+for line in lines:
+    tokens = parseExpressao(line)
+    print(tokens)
